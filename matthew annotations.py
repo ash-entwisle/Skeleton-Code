9e9b6341967b98661f4a1d0fe53920554949e298
matthew-townson <MJWTownson@gmail.com>
2022-01-06 11:52:03 +0000

    Updated files where neccessary and rearranged stuff (why did i duplicate main here fully???)

--- matthew annotations.py
+++ matthew annotations.py
@@ -2,6 +2,7 @@
 # this code should be used in conjunction with the Preliminary Material
 # written by the AQA Programmer Team
 # developed in the Python 3.9 programming environment
+#! Best read in BetterComments because nice colours yay!
 
 import random
 import os
